3ac716cd418d4ac8577a6ecd027f154bb5f098f9
Chris Thompson <g0kla@arrl.net>
2020-12-22 09:48:22 -0500

    Revert PFH addreplace algorithm

--- src/fileStore/SortedPfhArrayList.java
+++ src/fileStore/SortedPfhArrayList.java
@@ -14,12 +14,34 @@ public class SortedPfhArrayList extends SortedArrayList<PacSatFileHeader> {
 		return super.add(img);
 	}
 	
+	public boolean addOrReplace(PacSatFileHeader pfh) {
+		for (int i=0; i < this.size(); i++) {
+			if (get(i).getFileId() == pfh.getFileId()) {
+				if (pfh != null) {
+					// log the size and reset the state if we now have partial download
+					long existingDownloadedBytes = get(i).getFieldById(PacSatFileHeader.FILE_SIZE).getLongValue();
+					long downloadedBytes = pfh.getFieldById(PacSatFileHeader.FILE_SIZE).getLongValue();
+					pfh.copyMetaData(get(i));
+					// Reset the status if we thought this was fully downloaded.  Dont change otherwise
+					if ((pfh.state == PacSatFileHeader.MSG || pfh.state == PacSatFileHeader.NEWMSG)&& existingDownloadedBytes < downloadedBytes) {
+						pfh.state = PacSatFileHeader.PARTIAL;
+						pfh.downloadedBytes = existingDownloadedBytes;
+					}
+				}
+				// Delete the old one
+				this.remove(i);
+				return super.add(pfh);
+			}
+		}
+		return super.add(pfh);
+	}
+	
 	/**
 	 * Add this item or delete the existing item and insert a new one if it already exists
 	 * @param img
 	 * @return
 	 */
-	public boolean addOrReplace(PacSatFileHeader pfh) {
+	public boolean NEWaddOrReplace(PacSatFileHeader pfh) {
 		if (this.size() > 0) {
 			int i = Collections.binarySearch(this, pfh);
 			if (i >= 0) { // we found it
